3a59dbc769cd4501cf944998e4670f04c1a70ab0
linnefromice <linnefromice@gmail.com>
2025-02-26 12:10:35 +0900

    initialize app

---
+++ App/App/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by arata.haruyama on 2025/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
